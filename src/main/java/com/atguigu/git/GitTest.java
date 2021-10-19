197486f96018774379e7efc175c1c5378c579c2b
pjd <530391855@qq.com>
2021-10-19 22:30:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 潘江东
+ * @date 2021/10/19 - 22:25
+ */
+public class GitTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
